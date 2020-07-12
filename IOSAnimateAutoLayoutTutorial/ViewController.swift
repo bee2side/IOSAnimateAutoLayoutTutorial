a1ac3efe4678392e31ea068bae8c2a1e4e626737
dylan.k <bee2side@gmail.com>
2020-07-13 08:41:42 +0900

    view animation

--- IOSAnimateAutoLayoutTutorial/ViewController.swift
+++ IOSAnimateAutoLayoutTutorial/ViewController.swift
@@ -9,7 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
+    @IBAction func moveButtonPressed(_ sender: Any) {
+        let newConstraint = NSLayoutConstraint(item: redView as Any, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leading, multiplier: 1.0, constant: view.frame.width/2)
+        UIView.animate(withDuration: 2.0, delay: 0.0, options: .curveEaseOut, animations: {
+            self.view.removeConstraint(self.leadingConstraint)
+            self.view.addConstraint(newConstraint)
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+        leadingConstraint = newConstraint
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
